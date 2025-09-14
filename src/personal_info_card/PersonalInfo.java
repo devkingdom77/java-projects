81e68af46a699effd5e486e2e4b8c6e7fbf5bfe7
Abone kingdom <abonekingdom7@gmail.com>
2025-09-13 18:17:00 -0700

    personal-info-card

---
+++ src/personal_info_card/PersonalInfo.java
@@ -0,0 +1,31 @@
+package personal_info_card;
+import java.util.Scanner;
+
+public class PersonalInfo{
+  public static void main(String[] args){
+
+    // PERSONAL INFO CARD
+    // This project asks user for their name, age, and hobby
+    // Stores them in a variable and then print a formattable profile card
+
+    Scanner scanner = new Scanner(System.in);
+
+
+
+    System.out.print("Enter your name: ");
+    String name = scanner.next();
+
+    System.out.print("Enter your age: ");
+    int age = scanner.nextInt();
+
+    System.out.print("What is your hobby: ");
+    String hobby = scanner.next();
+
+
+    System.out.println();
+
+    System.out.println("Hello my name is " + name);
+    System.out.println("I am " + age + " years old");
+    System.out.println("I love " + hobby + " a lot");
+  }
+}
